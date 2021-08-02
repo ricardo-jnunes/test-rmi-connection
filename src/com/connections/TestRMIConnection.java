1fcd618c98349264aa6efdab22fa57eb298ebca3
Ricardo José Nunes <ricardo.nunes@compasso.com.br>
2021-08-02 15:06:05 -0300

    Adjustments to use generic connection

--- src/com/connections/TestRMIConnection.java
+++ src/com/connections/TestRMIConnection.java
@@ -1,6 +1,7 @@
 package com.connections;
 
 import java.rmi.Naming;
+import java.rmi.Remote;
 
 /**
  * 
@@ -9,28 +10,21 @@ import java.rmi.Naming;
  */
 public class TestRMIConnection {
 
-	private static final String RMI_SERVER_URL = "rmi://as1980:15061/RMI_SERVE";
-	   
-	public static void main(String args[]) throws Exception {	
-		
-        if (System.getSecurityManager() == null) {
-            System.setSecurityManager(new SecurityManager());
-        }
+	private static final String RMI_SERVER_URL = "rmi://<server_port>:<rmi_port>/RMI_SERVICE";
 
-        try {
-            System.out.println("Connecting to " + RMI_SERVER_URL);
-            
-            long beginTime = System.currentTimeMillis();
-            ExampleService service = (ExampleService) Naming.lookup(RMI_SERVER_URL);
-            long endTime = System.currentTimeMillis();
+	public static void main(String args[]) throws Exception {
 
-            System.out.println("end printing response"); 
-        }
-        catch (Exception e) {
-            //logger.error("Exception occured: " + e + " - " + RMI_SERVER_URL);
-            //logger.error(e);
-            e.printStackTrace();
-        }
-        System.out.println("The test complete");
-    }
+		try {
+			System.out.println("Connecting to " + RMI_SERVER_URL);
+
+			long beginTime = System.currentTimeMillis();
+			Remote service = (Remote) Naming.lookup(RMI_SERVER_URL);
+			long endTime = System.currentTimeMillis();
+
+			System.out.println("End printing response");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("The test complete");
+	}
 }
